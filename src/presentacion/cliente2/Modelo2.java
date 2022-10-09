0cd1b7720824d8628ef20d61689d7f6e1fc8ef99
KennyVeraF <keveraf@correo.udistrital.edu.co>
2022-10-09 17:11:45 -0500

    Se agregaron los 3 clientes (tarjetas)

---
+++ src/presentacion/cliente2/Modelo2.java
@@ -0,0 +1,224 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package presentacion;
+
+import java.io.IOException;
+import javax.swing.ImageIcon;
+import logica.ConexionCliente;
+import persistencia.Vehicular;
+
+/**
+ *
+ * @author cvelez
+ */
+public class Modelo {
+
+    private VistaSimulacion ventanaSimulacion;
+
+    private Vehicular semaforoVechicular;
+
+    public VistaSimulacion getVistaSimulacion() {
+
+        if (ventanaSimulacion == null) {
+            ventanaSimulacion = new VistaSimulacion(this);
+        }
+        return ventanaSimulacion;
+    }
+
+    public void iniciar() {
+        getVistaSimulacion().setSize(800, 900);
+        getVistaSimulacion().setVisible(true);
+        getVistaSimulacion().setLocationRelativeTo(null);
+       
+
+    }
+
+    public void conectarClientes(int numCruce) throws IOException {
+        ConexionCliente cliente = new ConexionCliente();
+        switch (numCruce) {
+            case 1: // Primer cruce
+
+                System.out.println("Usted eligió la opcion 1.");
+                //Iniciar cliente
+                cliente.ejecutarConexion("localhost", 5050, "cliente1");
+                
+                
+                break;
+
+            case 2: // Segundo cruce
+
+                System.out.println("Usted eligió la opcion 2.");
+
+                break;
+
+            case 3: // Tercer cruce
+
+                System.out.println("Usted eligió la opcion 3.");
+
+                break;
+
+            default:
+
+                System.out.println("Opcion incorrecta");
+
+        }
+    }
+    public String getEstado(){
+        return "luces ok";
+    }
+
+    /*
+    * Accion de encender o apagar la luz roja
+     */
+    void accionRojo(boolean accion, int grupo) {
+
+        switch (grupo) {
+            case 1:
+                if (accion) {
+                    getVistaSimulacion().getLbl_img1().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_true.png")));
+                    getVistaSimulacion().getLbl_img11().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_true.png")));
+                } else {
+                    getVistaSimulacion().getLbl_img1().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_false.png")));
+                    getVistaSimulacion().getLbl_img11().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_false.png")));
+                }   break;
+            case 2:
+                if (accion) {
+                    getVistaSimulacion().getLbl_grup2_image_red().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_true.png")));
+                    getVistaSimulacion().getLbl_grup2_image_red_2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_true.png")));
+                } else {
+                    getVistaSimulacion().getLbl_grup2_image_red().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_false.png")));
+                    getVistaSimulacion().getLbl_grup2_image_red_2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_false.png")));
+                }   break;
+            default:
+                if (accion) {
+                    getVistaSimulacion().getLbl_grupo3_imageRed().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_true.png")));
+                    getVistaSimulacion().getLbl_grupo3_imageRed2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_true.png")));
+                } else {
+                    getVistaSimulacion().getLbl_grupo3_imageRed().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_false.png")));
+                    getVistaSimulacion().getLbl_grupo3_imageRed2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_false.png")));
+                }   break;
+        }
+    }
+
+    /*
+    * Accion de encender o apagar la luz amarilla
+     */
+    void accionAmarilla(boolean accion, int grupo) {
+
+        switch (grupo) {
+            case 1:
+                if (accion) {
+                    getVistaSimulacion().getLbl_img2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/yellow_true.png")));
+                    getVistaSimulacion().getLbl_img22().setIcon(new ImageIcon(getClass().getResource("/Imagenes/yellow_true.png")));
+                } else {
+                    getVistaSimulacion().getLbl_img2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/yellow_false.png")));
+                    getVistaSimulacion().getLbl_img22().setIcon(new ImageIcon(getClass().getResource("/Imagenes/yellow_false.png")));
+                }   break;
+            case 2:
+                if (accion) {
+                    getVistaSimulacion().getLbl_grup2_image_yellow().setIcon(new ImageIcon(getClass().getResource("/Imagenes/yellow_true.png")));
+                    getVistaSimulacion().getLbl_grup2_image_yellow1().setIcon(new ImageIcon(getClass().getResource("/Imagenes/yellow_true.png")));
+                } else {
+                    getVistaSimulacion().getLbl_grup2_image_yellow().setIcon(new ImageIcon(getClass().getResource("/Imagenes/yellow_false.png")));
+                    getVistaSimulacion().getLbl_grup2_image_yellow1().setIcon(new ImageIcon(getClass().getResource("/Imagenes/yellow_false.png")));
+                }   break;
+        }
+    }
+
+    /*
+    * Accion de encender o apagar la luz verde
+     */
+    void accionVerde(boolean accion, int grupo) {
+
+        switch (grupo) {
+            case 1:
+                if (accion) {
+                    getVistaSimulacion().getLbl_img3().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_true.png")));
+                    getVistaSimulacion().getLbl_img33().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_true.png")));
+                } else {
+                    getVistaSimulacion().getLbl_img3().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_false.png")));
+                    getVistaSimulacion().getLbl_img33().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_false.png")));
+                }   break;
+            case 2:
+                if (accion) {
+                    getVistaSimulacion().getLbl_grup2_image_green().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_true.png")));
+                    getVistaSimulacion().getLbl_grup2_image_green_2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_true.png")));
+                } else {
+                    getVistaSimulacion().getLbl_grup2_image_green().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_false.png")));
+                    getVistaSimulacion().getLbl_grup2_image_green_2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_false.png")));
+                }   break;
+            default:
+                if (accion) {
+                    getVistaSimulacion().getLbl_grupo3_imageGreen().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_true.png")));
+                    getVistaSimulacion().getLbl_grupo3_imageGreen2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_true.png")));
+                } else {
+                    getVistaSimulacion().getLbl_grupo3_imageGreen().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_false.png")));
+                    getVistaSimulacion().getLbl_grupo3_imageGreen2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_false.png")));
+                }   break;
+        }
+
+    }
+
+    /*
+    * Accion de apagar todas las luces
+     */
+    void accionApagarTodo() {
+
+        //Apagar grupo 1
+        getVistaSimulacion().getLbl_img3().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_false.png")));
+        getVistaSimulacion().getLbl_img2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/yellow_false.png")));
+        getVistaSimulacion().getLbl_img1().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_false.png")));
+
+        // Apagar grupo 2
+        getVistaSimulacion().getLbl_grup2_image_green().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_false.png")));
+        getVistaSimulacion().getLbl_grup2_image_green_2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_false.png")));
+        getVistaSimulacion().getLbl_grup2_image_yellow().setIcon(new ImageIcon(getClass().getResource("/Imagenes/yellow_false.png")));
+        getVistaSimulacion().getLbl_grup2_image_red().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_false.png")));
+        getVistaSimulacion().getLbl_grup2_image_red_2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_false.png")));
+
+        //Desabilitar radiobuttons
+        getVistaSimulacion().getBtnRojo().setEnabled(false);
+        getVistaSimulacion().getBtnAmarillo().setEnabled(false);
+        getVistaSimulacion().getBtnVerde().setEnabled(false);
+
+        getVistaSimulacion().getBtnRojo_grup2().setEnabled(false);
+        getVistaSimulacion().getBtnAmarillo_grup2().setEnabled(false);
+        getVistaSimulacion().getBtnVerde_grup2().setEnabled(false);
+    }
+
+    /*
+    * Accion de apagar todas las luces
+     */
+    void accionEncenderTodo() {
+        //Apagar grupo 1
+        getVistaSimulacion().getLbl_img3().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_true.png")));
+        getVistaSimulacion().getLbl_img2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/yellow_true.png")));
+        getVistaSimulacion().getLbl_img1().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_true.png")));
+
+        // Apagar grupo 2
+        getVistaSimulacion().getLbl_grup2_image_green().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_true.png")));
+        getVistaSimulacion().getLbl_grup2_image_green_2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/green_true.png")));
+        getVistaSimulacion().getLbl_grup2_image_yellow().setIcon(new ImageIcon(getClass().getResource("/Imagenes/yellow_true.png")));
+        getVistaSimulacion().getLbl_grup2_image_red().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_true.png")));
+        getVistaSimulacion().getLbl_grup2_image_red_2().setIcon(new ImageIcon(getClass().getResource("/Imagenes/red_true.png")));
+
+        //Desabilitar radiobuttons
+        getVistaSimulacion().getBtnRojo().setEnabled(true);
+        getVistaSimulacion().getBtnAmarillo().setEnabled(true);
+        getVistaSimulacion().getBtnVerde().setEnabled(true);
+
+        getVistaSimulacion().getBtnRojo().setSelected(true);
+        getVistaSimulacion().getBtnAmarillo().setSelected(true);
+        getVistaSimulacion().getBtnVerde().setSelected(true);
+
+        getVistaSimulacion().getBtnRojo_grup2().setEnabled(true);
+        getVistaSimulacion().getBtnAmarillo_grup2().setEnabled(true);
+        getVistaSimulacion().getBtnVerde_grup2().setEnabled(true);
+
+        getVistaSimulacion().getBtnRojo_grup2().setSelected(true);
+        getVistaSimulacion().getBtnAmarillo_grup2().setSelected(true);
+        getVistaSimulacion().getBtnVerde_grup2().setSelected(true);
+    }
+}
---
+++ src/presentacion/cliente3/ControladorSimulacion3.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package presentacion;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class ControladorSimulacion implements ActionListener {
+
+    private final VistaSimulacion simulacion;
+    private final Modelo modelo;
+
+    public ControladorSimulacion(VistaSimulacion vista) {
+        this.simulacion = vista;
+        this.modelo = vista.getModelo();
+    }
+
+    public Modelo getModelo() {
+        return modelo;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() instanceof JRadioButton) {
+            JRadioButton radioButton = (JRadioButton) e.getSource();
+            if (radioButton == simulacion.getBtnRojo()) {
+                getModelo().accionRojo(radioButton.isSelected(), 1);
+            } else if (radioButton == simulacion.getBtnAmarillo()) {
+                getModelo().accionAmarilla(radioButton.isSelected(), 1);
+            } else if (radioButton == simulacion.getBtnVerde()) {
+                getModelo().accionVerde(radioButton.isSelected(), 1);
+            } else if (radioButton == simulacion.getBtnRojo_grup2()) {
+                getModelo().accionRojo(radioButton.isSelected(), 2);
+            } else if (radioButton == simulacion.getBtnAmarillo_grup2()) {
+                getModelo().accionAmarilla(radioButton.isSelected(), 2);
+            } else if (radioButton == simulacion.getBtnVerde_grup2()) {
+                getModelo().accionVerde(radioButton.isSelected(), 2);
+            }else if (radioButton == simulacion.getBtnRojo_grup3()){
+                getModelo().accionRojo(radioButton.isSelected(), 3);
+            }else if (radioButton == simulacion.getBtnVerde_grup3()){
+                getModelo().accionVerde(radioButton.isSelected(), 3);
+            }
+        } else if (e.getSource() instanceof JButton) {
+            JButton btnButton = (JButton) e.getSource();
+            if (btnButton == simulacion.getBtn_off()){
+                getModelo().accionApagarTodo();
+            } else if (btnButton == simulacion.getBtn_on()) {
+                getModelo().accionEncenderTodo();
+            }
+        }
+    }
+
+
+}
